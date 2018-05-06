8aad3d39031bae92ed1eda9573216c407067eb3d
Eric Lai <lyy6963@gmail.com>
2018-05-07 01:07:48 +0800

    修改get参数名称

--- index2.php
+++ index2.php
@@ -1,5 +1,5 @@
 <?php
-    if(!isset($_GET['name'])){
+    if(!isset($_GET['uuid'])){
         header("Location:index.php");
         exit;
     }
@@ -14,7 +14,7 @@
     <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src="core/js/IAT.js"></script>
     <script>
-        sub = <?php echo "'".$_GET['name']."';"?>
+        sub = <?php echo "'".$_GET['uuid']."';"?>
     </script>
     <script type="text/javascript">
         initialize();
